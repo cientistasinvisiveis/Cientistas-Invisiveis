21d160782941539ba4ea6cba2a4bcc94599236ff
Grazielly Galeano <graziellygaleano@gmail.com>
2026-08-29 10:18:13 -0300

    corrigi a chamada de renderIndex no router

--- backend/src/routes/indexRouter.js
+++ backend/src/routes/indexRouter.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { homeController } from '../controllers/homeController.js';
+import { renderIndex } from '../controllers/homeController.js'
 
 const router = Router();
 
 
-router.get('/', homeController.renderIndex);
+router.get('/', renderIndex);
 
 module.exports = router;
