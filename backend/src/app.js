4c7c8347d8f6ff189458ffc0bcb6a9ff5ce41e3a
Grazielly Galeano <graziellygaleano@gmail.com>
2026-08-29 10:48:23 -0300

    import de path

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import cors from 'cors';
 import pesquisadorRouter from './routes/pesquisadorRouter.js'; 
 import indexRoutes from './routes/indexRouter.js'
