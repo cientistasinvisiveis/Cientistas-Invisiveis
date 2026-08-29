b234696d7d2fa5d7bc8fe12a7756047587b6ad1c
Grazielly Galeano <graziellygaleano@gmail.com>
2026-08-29 10:50:54 -0300

    tentativa de usar o process.cwd()

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,14 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 app.set('view engine', 'ejs');
-
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+app.set('views', path.join(process.cwd(), 'backend', 'src', 'views'));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 
 app.use('/', indexRoutes);
-
 app.use('/api/pesquisadores', pesquisadorRouter);
 
 export default app;
